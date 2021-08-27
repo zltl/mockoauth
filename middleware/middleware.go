8effb974b37cadc6c5d22ec07b806cd5f36ef6be
liaotonglang <liaotonglang@gmail.com>
2021-08-27 17:03:18 +0800

    recover custom

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,8 +1,20 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
 
 func Init(r *gin.Engine) {
 	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
+	r.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
+		if err, ok := recovered.(string); ok {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
+			log.Errorf("error recovery: %s")
+		}
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}))
 }
